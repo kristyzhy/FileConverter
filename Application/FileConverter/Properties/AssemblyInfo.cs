c89bd63adb3ba6575a72272daf54a3974a505b25
Adrien Allard <adrien.allard.pro@gmail.com>
2026-02-13 21:47:37 +0100

    Chore: Change software year to 2026.

--- Application/FileConverter/Properties/AssemblyInfo.cs
+++ Application/FileConverter/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FileConverter")]
-[assembly: AssemblyCopyright("Copyright © 2025")]
+[assembly: AssemblyCopyright("Copyright © 2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
